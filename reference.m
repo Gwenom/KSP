cf5f2c569c7b440e45ece12d38c5132d4179fc0d
Gwendolyn <gwenthomasbruin@gmail.com>
2021-12-30 16:21:49 -0800

    Creation of Reference file with orbital infromation for celestial bodies

---
+++ reference.m
@@ -0,0 +1,227 @@
+Kerbol.R = 261600000 %Equitorial Radius in m
+Kerbol.m = 1.7565459e28 %Mass in kg
+Kerbol.mu = 1.1723328e19 %Standard gravitational parameter m^3/s^2
+
+Moho.a = 5263138304 %Semi-Major Axis in m
+Moho.ra = 6315765981 %radius of apoapsis in m
+Moho.rp = 4210510628 %radius of periapsis in m
+Moho.e = 0.2 %Eccentricity
+Moho.i = 7 %Inclination in deg
+Moho.AOP = 15 %Argument of periapsis in deg
+Moho.RAAN = 70 %Right ascension of the ascending node in deg
+Moho.M = 3.14 %Mean Anomaly at t=0 in rad
+Moho.T = 2215754 %Sideral Period in seconds
+Moho.R = 250000 %Equitorial radius in m
+Moho.m = 2.5263314e21 %Mass in kg
+Moho.mu = 1.6860938e11 %Standard gravitational parameter in m^3/s^2
+Moho.rSOI = 9646663 %Radius of the sphere of influence in m
+
+Eve.a = 9832684544 %Semi-Major Axis in m
+Eve.ra = 9931011387 %radius of apoapsis in m
+Eve.rp = 9734357701 %radius of periapsis in m
+Eve.e = 0.01 %Eccentricity
+Eve.i = 2.1 %Inclination in deg
+Eve.AOP = 0 %Argument of periapsis in deg
+Eve.RAAN = 15 %Right ascension of the ascending node in deg
+Eve.M = 3.14 %Mean Anomaly at t=0 in rad
+Eve.T = 5657995 %Sideral Period in seconds
+Eve.R = 700000 %Equitorial radius in m
+Eve.m = 1.2243980e23 %Mass in kg
+Eve.mu = 8.1717302e12 %Standard gravitational parameter in m^3/s^2
+Eve.rSOI = 85109365 %Radius of the sphere of influence in m
+
+Eve.Gilly.a = 31500000 %Semi-Major Axis in m
+Eve.Gilly.ra = 48825000 %radius of apoapsis in m
+Eve.Gilly.rp = 14175000 %radius of periapsis in m
+Eve.Gilly.e = 0.55 %Eccentricity
+Eve.Gilly.i = 12 %Inclination in deg
+Eve.Gilly.AOP = 10 %Argument of periapsis in deg
+Eve.Gilly.RAAN = 80 %Right ascension of the ascending node in deg
+Eve.Gilly.M = 0.9 %Mean Anomaly at t=0 in rad
+Eve.Gilly.T = 388587 %Sideral Period in seconds
+Eve.Gilly.R = 13000 %Equitorial radius in m
+Eve.Gilly.m = 1.2420363e17 %Mass in kg
+Eve.Gilly.mu = 8289449.8 %Standard gravitational parameter in m^3/s^2
+Eve.Gilly.rSOI = 126123.27 %Radius of the sphere of influence in m
+
+Kerbin.a = 13599840256 %Semi-Major Axis in m
+Kerbin.ra = 13599840256 %radius of apoapsis in m
+Kerbin.rp = 13599840256 %radius of periapsis in m
+Kerbin.e = 0 %Eccentricity
+Kerbin.i = 0 %Inclination in deg
+Kerbin.AOP = 0 %Argument of periapsis in deg
+Kerbin.RAAN = 0 %Right ascension of the ascending node in deg
+Kerbin.M = 3.14 %Mean Anomaly at t=0 in rad
+Kerbin.T = 9203545 %Sideral Period in seconds
+Kerbin.R = 600000 %Equitorial radius in m
+Kerbin.m = 5.2915158e22 %Mass in kg
+Kerbin.mu = 3.531600e12 %Standard gravitational parameter in m^3/s^2
+Kerbin.rSOI = 84159286 %Radius of the sphere of influence in m
+
+Kerbin.Mun.a = 12000000 %Semi-Major Axis in m
+Kerbin.Mun.ra = 12000000 %radius of apoapsis in m
+Kerbin.Mun.rp = 12000000 %radius of periapsis in m
+Kerbin.Mun.e = 0 %Eccentricity
+Kerbin.Mun.i = 0 %Inclination in deg
+Kerbin.Mun.AOP = 0 %Argument of periapsis in deg
+Kerbin.Mun.RAAN = 0 %Right ascension of the ascending node in deg
+Kerbin.Mun.M = 1.7 %Mean Anomaly at t=0 in rad
+Kerbin.Mun.T = 138984 %Sideral Period in seconds
+Kerbin.Mun.R = 200000 %Equitorial radius in m
+Kerbin.Mun.m = 9.7599066e20 %Mass in kg
+Kerbin.Mun.mu = 6.5138398e10 %Standard gravitational parameter in m^3/s^2
+Kerbin.Mun.rSOI = 2429559.1 %Radius of the sphere of influence in m
+
+Kerbin.Minmus.a = 47000000 %Semi-Major Axis in m
+Kerbin.Minmus.ra = 47000000 %radius of apoapsis in m
+Kerbin.Minmus.rp = 47000000 %radius of periapsis in m
+Kerbin.Minmus.e = 0 %Eccentricity
+Kerbin.Minmus.i = 6 %Inclination in deg
+Kerbin.Minmus.AOP = 38 %Argument of periapsis in deg
+Kerbin.Minmus.RAAN = 78 %Right ascension of the ascending node in deg
+Kerbin.Minmus.M = 0.9 %Mean Anomaly at t=0 in rad
+Kerbin.Minmus.T = 1077311 %Sideral Period in seconds
+Kerbin.Minmus.R = 60000 %Equitorial radius in m
+Kerbin.Minmus.m = 2.6457580e19 %Mass in kg
+Kerbin.Minmus.mu = 1.7658e9 %Standard gravitational parameter in m^3/s^2
+Kerbin.Minmus.rSOI = 2247428.4 %Radius of the sphere of influence in m
+
+Duna.a = 20726155264 %Semi-Major Axis in m
+Duna.ra = 21783189163 %radius of apoapsis in m
+Duna.rp = 19669121365 %radius of periapsis in m
+Duna.e = 0.051 %Eccentricity
+Duna.i = 0.06 %Inclination in deg
+Duna.AOP = 0 %Argument of periapsis in deg
+Duna.RAAN = 135.5 %Right ascension of the ascending node in deg
+Duna.M = 3.14 %Mean Anomaly at t=0 in rad
+Duna.T = 17315400 %Sideral Period in seconds
+Duna.R = 320000 %Equitorial radius in m
+Duna.m = 4.515427e21 %Mass in kg
+Duna.mu = 3.0136321e11 %Standard gravitational parameter in m^3/s^2
+Duna.rSOI = 47921949 %Radius of the sphere of influence in m
+
+Duna.Ike.a = 3200000 %Semi-Major Axis in m
+Duna.Ike.ra = 3296000 %radius of apoapsis in m
+Duna.Ike.rp = 3104000 %radius of periapsis in m
+Duna.Ike.e = 0.03 %Eccentricity
+Duna.Ike.i = 0.2 %Inclination in deg
+Duna.Ike.AOP = 0 %Argument of periapsis in deg
+Duna.Ike.RAAN = 0 %Right ascension of the ascending node in deg
+Duna.Ike.M = 1.7 %Mean Anomaly at t=0 in rad
+Duna.Ike.T = 65766.7 %Sideral Period in seconds
+Duna.Ike.R = 130000 %Equitorial radius in m
+Duna.Ike.m = 2.7821615e20 %Mass in kg
+Duna.Ike.mu = 1.8568369e10 %Standard gravitational parameter in m^3/s^2
+Duna.Ike.rSOI = 1049598.9 %Radius of the sphere of influence in m
+
+Dres.a = 40839348203 %Semi-Major Axis in m
+Dres.ra = 46761053692 %radius of apoapsis in m
+Dres.rp = 34917642714 %radius of periapsis in m
+Dres.e = 0.145 %Eccentricity
+Dres.i = 5 %Inclination in deg
+Dres.AOP = 90 %Argument of periapsis in deg
+Dres.RAAN = 280 %Right ascension of the ascending node in deg
+Dres.M = 3.14 %Mean Anomaly at t=0 in rad
+Dres.T = 11392903.3 %Sideral Period in seconds
+Dres.R = 138000 %Equitorial radius in m
+Dres.m = 3.2190937e20 %Mass in kg
+Dres.mu = 2.1484489e10 %Standard gravitational parameter in m^3/s^2
+Dres.rSOI = 32832840 %Radius of the sphere of influence in m
+
+Jool.a = 68773560320 %Semi-Major Axis in m
+Jool.ra = 72212238387 %radius of apoapsis in m
+Jool.rp = 65334882253 %radius of periapsis in m
+Jool.e = 0.05 %Eccentricity
+Jool.i = 1.304 %Inclination in deg
+Jool.AOP = 0 %Argument of periapsis in deg
+Jool.RAAN = 52 %Right ascension of the ascending node in deg
+Jool.M = 0.1 %Mean Anomaly at t=0 in rad
+Jool.T = 104661432 %Sideral Period in seconds
+Jool.R = 6000000 %Equitorial radius in m
+Jool.m = 4.2332127e24 %Mass in kg
+Jool.mu = 2.82528e14 %Standard gravitational parameter in m^3/s^2
+Jool.rSOI = 2.4559852e9 %Radius of the sphere of influence in m
+
+Jool.Pol.a = 179890000 %Semi-Major Axis in m
+Jool.Pol.ra = 210624207 %radius of apoapsis in m
+Jool.Pol.rp = 149155794 %radius of periapsis in m
+Jool.Pol.e = 0.171 %Eccentricity
+Jool.Pol.i = 4.25 %Inclination in deg
+Jool.Pol.AOP = 15 %Argument of periapsis in deg
+Jool.Pol.RAAN = 2 %Right ascension of the ascending node in deg
+Jool.Pol.M = 0.9 %Mean Anomaly at t=0 in rad
+Jool.Pol.T = 909742.2 %Sideral Period in seconds
+Jool.Pol.R = 44000 %Equitorial radius in m
+Jool.Pol.m = 1.0813507e19 %Mass in kg
+Jool.Pol.mu = 7.2170208e8 %Standard gravitational parameter in m^3/s^2
+Jool.Pol.rSOI = 1042138.9 %Radius of the sphere of influence in m
+
+Jool.Bop.a = 128500000 %Semi-Major Axis in m
+Jool.Bop.ra = 158697500 %radius of apoapsis in m
+Jool.Bop.rp = 98302500 %radius of periapsis in m
+Jool.Bop.e = 0.235 %Eccentricity
+Jool.Bop.i = 15 %Inclination in deg
+Jool.Bop.AOP = 25 %Argument of periapsis in deg
+Jool.Bop.RAAN = 10 %Right ascension of the ascending node in deg
+Jool.Bop.M = 0.9 %Mean Anomaly at t=0 in rad
+Jool.Bop.T = 544507 %Sideral Period in seconds
+Jool.Bop.R = 65000 %Equitorial radius in m
+Jool.Bop.m = 3.7261090e19 %Mass in kg
+Jool.Bop.mu = 2.4868349e9 %Standard gravitational parameter in m^3/s^2
+Jool.Bop.rSOI = 1221060.9 %Radius of the sphere of influence in m
+
+Jool.Tylo.a = 68500000 %Semi-Major Axis in m
+Jool.Tylo.ra = 68500000 %radius of apoapsis in m
+Jool.Tylo.rp = 68500000 %radius of periapsis in m
+Jool.Tylo.e = 0 %Eccentricity
+Jool.Tylo.i = 0.025 %Inclination in deg
+Jool.Tylo.AOP = 0 %Argument of periapsis in deg
+Jool.Tylo.RAAN = 0 %Right ascension of the ascending node in deg
+Jool.Tylo.M = 3.14 %Mean Anomaly at t=0 in rad
+Jool.Tylo.T = 211926 %Sideral Period in seconds
+Jool.Tylo.R = 600000 %Equitorial radius in m
+Jool.Tylo.m = 4.2332127e22 %Mass in kg
+Jool.Tylo.mu = 2.82528e12 %Standard gravitational parameter in m^3/s^2
+Jool.Tylo.rSOI = 10856518 %Radius of the sphere of influence in m
+
+Jool.Vall.a = 43152000 %Semi-Major Axis in m
+Jool.Vall.ra = 43152000 %radius of apoapsis in m
+Jool.Vall.rp = 43152000 %radius of periapsis in m
+Jool.Vall.e = 0 %Eccentricity
+Jool.Vall.i = 0 %Inclination in deg
+Jool.Vall.AOP = 0 %Argument of periapsis in deg
+Jool.Vall.RAAN = 0 %Right ascension of the ascending node in deg
+Jool.Vall.M = 0.9 %Mean Anomaly at t=0 in rad
+Jool.Vall.T = 105962 %Sideral Period in seconds
+Jool.Vall.R = 300000 %Equitorial radius in m
+Jool.Vall.m = 3.1087655e21 %Mass in kg
+Jool.Vall.mu = 2.0748150e11 %Standard gravitational parameter in m^3/s^2
+Jool.Vall.rSOI = 2406401.4 %Radius of the sphere of influence in m
+
+Jool.Laythe.a = 27184000 %Semi-Major Axis in m
+Jool.Laythe.ra = 27184000 %radius of apoapsis in m
+Jool.Laythe.rp = 27184000 %radius of periapsis in m
+Jool.Laythe.e = 0 %Eccentricity
+Jool.Laythe.i = 0 %Inclination in deg
+Jool.Laythe.AOP = 0 %Argument of periapsis in deg
+Jool.Laythe.RAAN = 0 %Right ascension of the ascending node in deg
+Jool.Laythe.M = 3.14 %Mean Anomaly at t=0 in rad
+Jool.Laythe.T = 52981 %Sideral Period in seconds
+Jool.Laythe.R = 500000 %Equitorial radius in m
+Jool.Laythe.m = 2.9397311e22 %Mass in kg
+Jool.Laythe.mu = 1.962e12 %Standard gravitational parameter in m^3/s^2
+Jool.Laythe.rSOI = 3723645.8 %Radius of the sphere of influence in m
+
+Eeloo.a = 90118820000 %Semi-Major Axis in m
+Eeloo.ra = 113549713200 %radius of apoapsis in m
+Eeloo.rp = 66687926800 %radius of periapsis in m
+Eeloo.e = 0.26 %Eccentricity
+Eeloo.i = 6.15 %Inclination in deg
+Eeloo.AOP = 260 %Argument of periapsis in deg
+Eeloo.RAAN = 50 %Right ascension of the ascending node in deg
+Eeloo.M = 3.14 %Mean Anomaly at t=0 in rad
+Eeloo.T = 156992048 %Sideral Period in seconds
+Eeloo.R = 210000 %Equitorial radius in m
+Eeloo.m = 1.1149224e21 %Mass in kg
+Eeloo.mu = 7.4410815e10 %Standard gravitational parameter in m^3/s^2
+Eeloo.rSOI = 1.1908294e8 %Radius of the sphere of influence in m
